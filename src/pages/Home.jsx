e1e0b71590f69cac8edfca5891e0ad288451b333
HenriqueSoubhia <125106946+HenriqueSoubhia@users.noreply.github.com>
2023-08-17 23:08:53 -0300

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Banner from "../components/Banner";
 import Main from '../components/Main'
 import Footer from "../components/Footer";
 import SobeButton from "../components/SobeButton";
+import VLibras from '@djpfs/react-vlibras'
 
 const Home = () => {
 
 
   return (
     <>
+      <VLibras forceOnload={true}/>
       <Navbar />
       <Banner />
       <Main/>
